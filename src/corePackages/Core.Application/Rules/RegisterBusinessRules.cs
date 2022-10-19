db2b3412a1455072021c9f09b94dfab1aff46242
Yusuf Çırak <yusuf10901@hotmail.com>
2022-10-19 14:05:43 +0300

    BaseBusinessRules and registration for business rules added

---
+++ src/corePackages/Core.Application/Rules/RegisterBusinessRules.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Application.Rules;
+
+public static class RegisterBusinessRules
+{
+    public static IServiceCollection AddSubClassesOfType(this IServiceCollection services,Assembly assembly,Type type,Func<IServiceCollection,Type,IServiceCollection>? addWithLifeCycle=null)
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t);
+
+        if (addWithLifeCycle is null)
+        {
+            foreach (Type businessRuleType in types)
+            {
+                services.AddScoped(businessRuleType);
+            }
+        }
+        else
+        {
+            foreach (Type businessRuleType in types)
+            {
+                addWithLifeCycle(services, businessRuleType);
+            }
+        }
+
+
+        return services;
+
+    }
+}
